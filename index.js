254aa5ff95f6d01e6adff6dbad3dede12ff01a70
homershie <homershie@gmail.com>
2025-08-11 11:28:18 +0800

    Revert "Fix Cloudflare 522 (Connection timed out)#3"
    This reverts commit daf3981103aed6420aeea7f961cc1832b7c3839b.

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ const configureSession = () => {
     saveUninitialized: true, // 改為 true，確保未初始化的 session 也被保存
     cookie: {
       httpOnly: true,
-      sameSite: 'lax',
+      sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
       secure: process.env.NODE_ENV === 'production',
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 天
     },
